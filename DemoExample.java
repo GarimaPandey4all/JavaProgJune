cfe0221a45f21f972c8bffce034e1ec4d9d1fec8
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-06-16 15:04:06 +0530

    Add files via upload

---
+++ DemoExample.java
@@ -0,0 +1,8 @@
+class DemoExample {
+    public static void main(String[] args) { // main() - method / Entry point
+        System.out.println("Hello World");
+    }
+    public static void main() { // main() - method / Entry point
+        System.out.println("Hello World");
+    }
+}
