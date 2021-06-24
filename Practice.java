f5eb42081c85876e48b5ceb6de969937d95cedda
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-06-24 17:34:57 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,11 @@
+class Practical {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static void main() {
+        System.out.println("Hello World");
+    }
+    
+}
